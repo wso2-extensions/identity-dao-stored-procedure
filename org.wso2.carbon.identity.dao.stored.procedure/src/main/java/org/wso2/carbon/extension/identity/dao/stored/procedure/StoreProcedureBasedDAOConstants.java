fb4027dbff38ae51ae74d53408a503d5249e61a9
senthalan <senthalank@gmail.com>
2021-09-17 09:39:49 +0530

    Update data type name format

--- org.wso2.carbon.identity.dao.stored.procedure/src/main/java/org/wso2/carbon/extension/identity/dao/stored/procedure/StoreProcedureBasedDAOConstants.java
+++ org.wso2.carbon.identity.dao.stored.procedure/src/main/java/org/wso2/carbon/extension/identity/dao/stored/procedure/StoreProcedureBasedDAOConstants.java
@@ -35,6 +35,6 @@ public class StoreProcedureBasedDAOConstants {
     protected static final String ARG_TENANT_ID = "tenantId";
     protected static final String ARG_SCOPES = "SCOPES";
     protected static final String ARG_CLAIMS = "CLAIMS";
-    protected static final String DATA_TYPE_IDN_OAUTH_2_SCOPE = "IDN_OAUTH2_SCOPE";
-    protected static final String DATA_TYPE_IDN_OIDC_SCOPE_CLAIMS = "IDN_OIDC_SCOPE_CLAIMS";
+    protected static final String DATA_TYPE_IDN_OAUTH_2_SCOPE = "DT_IDN_OAUTH2_SCOPE";
+    protected static final String DATA_TYPE_IDN_OIDC_SCOPE_CLAIMS = "DT_IDN_OIDC_SCOPE_CLAIMS";
 }
